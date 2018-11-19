934af972af534a37bd6d25d679d7544850d6e14a
Riccardo Audano <riccardo.at.work@gmail.com>
2018-11-19 22:37:41 +0100

    added sort algorithms and relative tests, Also showing xunit theories

--- HelloDotNet/Program.cs
+++ HelloDotNet/Program.cs
@@ -7,15 +7,10 @@ namespace HelloDotNet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // Iterations();
-            //Recursion(1);
-            //  FibonacciIterative();
-            // FibonacciRecursion();
-            Arrays();
-       
+
         }
 
-        
+
 
         static void Conditionals()
         {
@@ -36,28 +31,28 @@ namespace HelloDotNet
         }
         static void Iterations()
         {
-            for(int i=1;i<=10;i++)
+            for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine(i);
             }
             int k = 1;
-            while(k<=10)
+            while (k <= 10)
             {
                 Console.WriteLine(k++);
-               // k++;
+                // k++;
             }
             k = 1;
             do
             {
                 Console.WriteLine(k++);
             }
-            while (k<=10);
+            while (k <= 10);
         }
         static void Recursion(int n)
         {
             Console.WriteLine(n);
             n++;
-            if (n<=10)
+            if (n <= 10)
             {
                 Recursion(n);
             }
@@ -68,7 +63,7 @@ namespace HelloDotNet
             int n2 = 1;
             Console.WriteLine(n1);
             Console.WriteLine(n2);
-            for(int i=0;i<8;i++)
+            for (int i = 0; i < 8; i++)
             {
                 int somma = n1 + n2;
                 n1 = n2;
@@ -78,7 +73,7 @@ namespace HelloDotNet
         }
         private static void FibonacciRecursion()
         {
-           for(int i=0;i<10;i++)
+            for (int i = 0; i < 10; i++)
             {
                 int fib = Fibo(i);
                 Console.WriteLine(fib);
@@ -87,11 +82,11 @@ namespace HelloDotNet
 
         private static int Fibo(int i)
         {
-            if(i==0)
+            if (i == 0)
             {
                 return 0;
             }
-            if(i==1)
+            if (i == 1)
             {
                 return 1;
             }
@@ -99,12 +94,11 @@ namespace HelloDotNet
         }
         static void Arrays()
         {
-            int n;
             int[] myNumbers = new int[10];
-            for(int i=0;i<myNumbers.Length;i++)
+            for (int i = 0; i < myNumbers.Length; i++)
             {
                 //n = myNumbers[i];
-                myNumbers[i] = (int) Math.Pow(2, i);
+                myNumbers[i] = (int)Math.Pow(2, i);
                 Console.WriteLine(myNumbers[i]);
             }
             // myNumbers[myNumbers.Length - 1] = 0;
@@ -123,9 +117,9 @@ namespace HelloDotNet
         }
         static bool IsSorted(int[] array)
         {
-            for(int i=1;i<array.Length;++i)
+            for (int i = 1; i < array.Length; ++i)
             {
-                if (array[i]<array[i-1])
+                if (array[i] < array[i - 1])
                 {
                     return false;
                 }
@@ -136,7 +130,7 @@ namespace HelloDotNet
         {
             foreach (int item in array)
             {
-                if (item==a)
+                if (item == a)
                 {
                     return true;
                 }
@@ -145,7 +139,7 @@ namespace HelloDotNet
         }
         public static bool BinarySearch(int[] array, int a, int start, int end)
         {
-            if (end<start)
+            if (end < start)
             {
                 return false;
             }
@@ -157,12 +151,143 @@ namespace HelloDotNet
             }
             else if (pivot < a)
             {
-                return BinarySearch(array, a, pos+1, end);
+                return BinarySearch(array, a, pos + 1, end);
             }
             else
             {
-                return BinarySearch(array, a, start, pos-1);
+                return BinarySearch(array, a, start, pos - 1);
             }
+        }
+
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(array, j, j + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int j, int v)
+        {
+            var temp = array[j];
+            array[j] = array[v];
+            array[v] = temp;
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var minPos = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minPos])
+                    {
+                        Swap(array, minPos, j);
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int value = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > value)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = value;
+            }
+        }
+
+
+        public static void MergeSort(int[] array)
+        {
+            MergeSort(array, 0, array.Length - 1);
+        }
+
+        private static void MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                MergeSort(array, left, middle);
+                MergeSort(array, middle + 1, right);
+                int a = left;
+                int b = middle + 1;
+                int k = 0;
+                int[] temp = new int[right - left + 1];
+                while (a <= middle && b <= right)
+                {
+                    if (array[a] < array[b])
+                    {
+                        temp[k] = array[a++];
+                    }
+                    else
+                    {
+                        temp[k] = array[b++];
+                    }
+                    k++;
+                }
+
+                while (a <= middle)
+                {
+                    temp[k++] = array[a++];
+                }
+                while (b <= right)
+                {
+                    temp[k++] = array[b++];
+                }
+                Array.Copy(temp, 0, array, left, temp.Length);
+            }
+
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSort(int[] array, int start, int end)
+        {
+            if (start > end)
+            {
+                return;
+            }
+
+            int left = start;
+            int right = end;
+            while (left <= right)
+            {
+                while (array[right] > array[start])
+                {
+                    right--;
+                }
+                while (left <= right && array[left] <= array[start])
+                {
+                    left++;
+                }
+                if (left < right)
+                {
+                    Swap(array, left, right);
+                    left++;
+                    right--;
+                }
+            }
+            Swap(array, start, right);
+            QuickSort(array, start, right - 1);
+            QuickSort(array, right + 1, end);
         }
     }
 
--- HelloDotNetTest/TestAlgoritmi.cs
+++ HelloDotNetTest/TestAlgoritmi.cs
@@ -1,19 +1,102 @@
-using System;
-using Xunit;
+using FluentAssertions;
 using HelloDotNet;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
 
 namespace HelloDotNetTest
 {
+
+
+    public class TestDataGenerator : IEnumerable<object[]>
+    {
+        private readonly List<object[]> data = new List<object[]>
+        {
+            new object[] {new int[] { 1, 65, 23, 7, 89, 90, 91, 5, 2, 12 }},
+            new object[] {new int[] { 54, 4545, 90, 383, 11111, 2083, 4545, 3, 2 }}
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+
+    public class TestDataGeneratorSorted : IEnumerable<object[]>
+    {
+        private readonly List<object[]> data = new List<object[]>
+        {
+            new object[] {new int[] { 1, 4, 6, 9, 11, 23, 45, 80 }},
+            new object[] {new int[] { 54, 80, 342, 4324, 1234, 3333, 8399, 36524, 1923764}}
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
     public class TestAlgoritmi
     {
-        [Fact]
-        public void TestBinarySearch()
+        [Theory]
+        [ClassData(typeof(TestDataGeneratorSorted))]
+        public void TestBinarySearch(int[] testInts)
         {
-            int[] testInts = {1,4,6,9,11,23,45,80};
+
+            //int[] testInts = { 1, 4, 6, 9, 11, 23, 45, 80 };
             bool found = Program.BinarySearch(testInts, 80, 0, testInts.Length - 1);
             Assert.True(found);
             found = Program.BinarySearch(testInts, 79, 0, testInts.Length - 1);
             Assert.False(found);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void TestBubbleSort(int[] testInts)
+        {
+          
+            testInts.Should().NotBeAscendingInOrder();
+            Program.BubbleSort(testInts);
+            testInts.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void TestSelectionSort(int[] testInts)
+        {
+           
+            testInts.Should().NotBeAscendingInOrder();
+            Program.SelectionSort(testInts);
+            testInts.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void TestInsertionSort(int[] testInts)
+        {
+    
+            testInts.Should().NotBeAscendingInOrder();
+            Program.InsertionSort(testInts);
+            testInts.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void TestMergeSort(int[] testInts)
+        {
+        
+            testInts.Should().NotBeAscendingInOrder();
+            Program.MergeSort(testInts);
+            testInts.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void TestQuickSort(int[] testInts)
+        {
+       
+            testInts.Should().NotBeAscendingInOrder();
+            Program.QuickSort(testInts);
+            testInts.Should().BeInAscendingOrder();
         }
     }
 }
